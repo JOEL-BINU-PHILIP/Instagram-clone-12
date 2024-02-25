0e8953acb61681f3747bc3e6599c4b039ec78c0f
JOEL-BINU-PHILIP <joelbinuphilip@gmail.com>
2024-02-25 23:47:40 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -5,19 +5,22 @@ import 'package:instagram_clone/firebase_options.dart';
 import 'package:instagram_clone/responsive/mobileScreenLayout.dart';
 import 'package:instagram_clone/responsive/responsive_layout_screen.dart';
 import 'package:instagram_clone/responsive/webScreenLayout.dart';
+import 'package:instagram_clone/screens/login_screen.dart';
 import 'package:instagram_clone/utils/colors.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   if (kIsWeb) {
     await Firebase.initializeApp(
-      options: FirebaseOptions(apiKey: 'AIzaSyBWURUQ2TVgGa-fKc0X_zaJdWyDEI-Gw6g', appId: '1:144935331171:web:8f90897e5ae8e7e0b75f56', messagingSenderId: '144935331171', projectId: 'instagram-clone-88349owkdj' , storageBucket: 'instagram-clone-88349owkdj.appspot.com')
-    );
-  }
-  else{
-      await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform
-  );
+        options: const FirebaseOptions(
+            apiKey: 'AIzaSyBWURUQ2TVgGa-fKc0X_zaJdWyDEI-Gw6g',
+            appId: '1:144935331171:web:8f90897e5ae8e7e0b75f56',
+            messagingSenderId: '144935331171',
+            projectId: 'instagram-clone-88349owkdj',
+            storageBucket: 'instagram-clone-88349owkdj.appspot.com'));
+  } else {
+    await Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform);
   }
   runApp(const MyApp());
 }
@@ -29,9 +32,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Instagram Clone',
-      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: mobileBackgroundColor),
-      home: ResponsiveLayout(webScreenLayout: WebScreenLayout() , mobileScreenLayout:MobileScreenLayout())
-    );
+        title: 'Instagram Clone',
+        theme: ThemeData.dark()
+            .copyWith(scaffoldBackgroundColor: mobileBackgroundColor),
+        // home: ResponsiveLayout(
+        //     webScreenLayout: WebScreenLayout(),
+        //     mobileScreenLayout: MobileScreenLayout())
+        home: LoginScreen(),
+        );
+
   }
 }
---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 32),
+          width: double.infinity,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              //svg image
+              Image.asset('assets/instagram.png' , height: 64,)
+              //text field input for email
+              //text field for password 
+              //button Login 
+              //Transitioning to signing up
+            ],
+          ),
+        )
+      ),
+    );
+  }
+}
